edd5df7aa4b5f97e7604281745cf1e96f27a78c1
Adi biton <adi.biton@gmail.com>
2011-11-15 20:55:08 +0200

    add test.php

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+	$opts = array(
+		'http'=>array(
+			'method'	=>	"POST",
+			'content'	=>	'track=flickr',
+			'header'	=>  "Content-Type: application/x-www-form-urlencoded\r\n"
+		)
+	);
+	$context = stream_context_create($opts);
+	$instream = fopen('https://Adibiton:dkhk11@stream.twitter.com/1/statuses/filter.json','r' ,false, $context);
+	while(! feof($instream)) {
+		if(! ($line = stream_get_line($instream, 20000, "\n"))) {
+			continue;
+		}else{
+			//print_r(json_decode($line));
+			$tweet = json_decode($line);
+			print $tweet->{'text'}."\n";
+			flush();
+		}
+	}
+?>
